9aa874766d58252ab89ba2fc44533ea9278402e7
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-14 16:24:18 +0100

    commit for history making

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 
 //creo lista array delle immagini
 const images = [
-  'C:/File HTML/Corso_Boolean/js-array-carousel/assets/img/01.webp',
-  'C:/File HTML/Corso_Boolean/js-array-carousel/assets/img/02.webp',
-  'C:/File HTML/Corso_Boolean/js-array-carousel/assets/img/03.webp',
-  'C:/File HTML/Corso_Boolean/js-array-carousel/assets/img/04.webp'
+  './assets/img/01.webp',
+  './assets/img/02.webp',
+  './assets/img/03.webp',
+  './assets/img/04.webp'
 ]
 
 //seleziono l'ordine per l'immagine attiva
 let activeImage = 0;
 
 //seleziono l'elemento in DOM dove inserire le immagini
-const imagesElement = document.querySelector('.row > .images')
+const imagesElement = document.querySelector('.col > .images')
 console.log(imagesElement);
 
 //loop dell'immagine e aggiungo una nuova immagine alla DOM
